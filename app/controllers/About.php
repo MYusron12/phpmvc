8a3a7b0b07f90fb8e48e20d8c2717e64b0716f8a
MYusron12 <ryusron19@gmail.com>
2020-01-26 20:10:24 +0700

    tambahan materi view dan materi assets

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -4,10 +4,19 @@ class About extends Controller
 {
 	public function index($nama = 'Yusron', $pekerjaan = 'Mahasiswa', $umur = 27)
 	{
-		$this->view('about/index');
+		$data['nama']= $nama;
+		$data['pekerjaan']=$pekerjaan;
+		$data['umur']=$umur;
+		$data['judul']='About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 	public function page()
 	{
+		$data['judul']='Pages';
+		$this->view('templates/header', $data);
 		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
-</head>
-
-<body>
-    <h1>About Me</h1>
-</body>
-
-</html>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/1.png" alt="Muhammad Yusron" width="200" class="rounded-circle shadow">
+    <p>hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
